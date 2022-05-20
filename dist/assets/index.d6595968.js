be12af4d8a274766f1dada00de95c6773c176c1b
zhenorzz <zhenorzz@gmail.com>
2022-05-20 16:55:32 +0800

    A dist

---
+++ dist/assets/index.d6595968.js
@@ -0,0 +1,1 @@
+import{d as a,b as n,a as u,z as p}from"./index.ffb5bdad.js";const d=a({setup(){const t=n(),e=u(),{params:s,query:o}=t,{path:r}=s;e.push({path:"/"+r,query:o})},render(){return p("div")}});export{d as default};
